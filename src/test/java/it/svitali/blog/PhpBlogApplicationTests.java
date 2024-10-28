38201f52a26eab3b00979ca60caa6649f8ede4c5
StefaniaVitali <stefy.egypt@hotmail.it>
2024-10-28 11:48:11 +0100

    Initial commit

---
+++ src/test/java/it/svitali/blog/PhpBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package it.svitali.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhpBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
